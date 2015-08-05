196e9a23b43e7fe01ebf3e75064b77d420e3c60b
Iain Porter <changeant@gmail.com>
2015-08-05 17:12:56 +0100

    Add SdoCapacity Test

--- src/main/java/biz/c24/io/demo/Utils.java
+++ src/main/java/biz/c24/io/demo/Utils.java
@@ -1,9 +1,14 @@
 package biz.c24.io.demo;
 
+
 import org.plei.prelei_schema.xsd.LEIRegistration;
+
+import java.nio.ByteBuffer;
 
 public class Utils {
 
+    private static int idOffset = -1;
+    private static org.plei.prelei_schema.xsd.sdo.LEIRegistration sdo = null;
 
     public static LEIRegistration cloneWithIdentifier(LEIRegistration cdo, int id) {
         LEIRegistration ret;
@@ -20,5 +25,38 @@ public class Utils {
         return ret;
     }
 
+    public static org.plei.prelei_schema.xsd.sdo.LEIRegistration cloneSdoWithIdentifier(org.plei.prelei_schema.xsd.sdo.LEIRegistration sdo, int id) {
+        byte[] bytes = sdo.getSdoData().clone();
 
+        if(sdo != Utils.sdo) {
+            byte[] pattern = sdo.getLegalEntityIdentifier().getBytes();
+            int startOffset = -1;
+            for(int i=0; i < bytes.length; i++) {
+                startOffset = i;
+                for(int j = 0; j < pattern.length && j+i < bytes.length; j++) {
+                    if(bytes[i+j] != pattern[j]) {
+                        startOffset = -1;
+                        break;
+                    }
+                }
+                if(startOffset >= 0) {
+                    break;
+                }
+            }
+
+            Utils.sdo = sdo;
+            idOffset = startOffset;
+        }
+
+        char[] newId = String.format("%020x", id).toCharArray();
+
+        for(int i = 0; i < newId.length; i++) {
+            bytes[idOffset + i] = (byte) newId[i];
+        }
+
+        org.plei.prelei_schema.xsd.sdo.LEIRegistration sdo2 = new org.plei.prelei_schema.xsd.sdo.LEIRegistration(bytes, 1);
+        return sdo2;
+
+
+    }
 }
--- src/main/java/biz/c24/io/demo/ehcache/SdoCapacityTest.java
+++ src/main/java/biz/c24/io/demo/ehcache/SdoCapacityTest.java
@@ -1,57 +1,69 @@
 package biz.c24.io.demo.ehcache;
 
+import java.io.File;
+import java.io.IOException;
+
+
 import biz.c24.io.api.C24;
 import biz.c24.io.demo.Utils;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.SearchAttribute;
+import net.sf.ehcache.config.Searchable;
 import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
-import org.plei.prelei_schema.xsd.LEIDirectory;
+import org.plei.prelei_schema.xsd.sdo.LEIDirectory;
 
-import java.io.File;
+public class SdoCapacityTest implements Runnable {
 
-public class CdoCapacityTest implements Runnable {
-    
     public static void main(String[] args) {
-        new CdoCapacityTest().run();
+        new SdoCapacityTest().run();
     }
-    
+
     public void run() {
-        
+
         try {
-        
+
             CacheManager manager = CacheManager.create();
-            
-            Cache cache = new Cache(
-                    new CacheConfiguration("testCache", Integer.MAX_VALUE)
-                      .memoryStoreEvictionPolicy(MemoryStoreEvictionPolicy.FIFO)
-                      .eternal(true)
-                      .timeToLiveSeconds(0)
-                      .timeToIdleSeconds(0));
+
+            CacheConfiguration cacheConfig = new CacheConfiguration("testCache", Integer.MAX_VALUE)
+                    .memoryStoreEvictionPolicy(MemoryStoreEvictionPolicy.FIFO)
+                    .eternal(true)
+                    .timeToLiveSeconds(0)
+                    .timeToIdleSeconds(0);
+
+            Searchable searchable = new Searchable();
+            cacheConfig.addSearchable(searchable);
+
+            searchable.addSearchAttribute(new SearchAttribute().name("legalEntityIdentifier").expression("value.getLegalEntityIdentifier()"));
+
+
+            Cache cache = new Cache(cacheConfig);
             manager.addCache(cache);
-            
-            LEIDirectory cdo = C24.parse(LEIDirectory.class).from(new File("/lei_sample.xml"));
-            
+
+
+            LEIDirectory sdo = C24.parse(LEIDirectory.class).from(new File("/lei_sample.xml"));
+
             int i;
             for(i = 0; i < Integer.MAX_VALUE; i++) {
-                cache.put(new Element(i, Utils.cloneWithIdentifier(cdo.getLEIRegistrations().getLEIRegistration(0), i)));
+                cache.put(new Element(i, Utils.cloneSdoWithIdentifier(sdo.getLEIRegistrations().getLEIRegistration(0), i)));
                 if(cache.getSize() != i+1) {
                     // We're full
                     break;
                 }
-                
+
                 if(i % 10000 == 0) {
                     System.out.println(i);
                 }
                 if(i % 100000 == 0) {
                     System.out.println("Heap Size (in Bytes): " + cache.getStatistics().getLocalHeapSizeInBytes());
                 }
-                
+
             }
-            
+
             System.out.println("Capacity " + i);
-            
+
             CacheManager.getInstance().shutdown();
         } catch(Exception ex) {
             ex.printStackTrace();
